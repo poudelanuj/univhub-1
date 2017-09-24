034d5e3ab5763ff4108e8d69f6092f9d70edb21e
bipinkh <bipinkh91@gmail.com>
2017-09-24 13:08:14 +0545

    final from me

--- univhub/urls.py
+++ univhub/urls.py
@@ -2,11 +2,9 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
-import notifications.urls
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'sadmin/',include('sadmin.urls')),
-    url('^inbox/notifications/', include(notifications.urls, namespace='notifications')),
 ]

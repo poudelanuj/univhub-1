6e95b231a41e487779029a267f590a253cabba39
enish paneru <theboyenish@gmail.com>
2017-10-04 10:46:57 +0545

    corrected errornous directories

--- univhub/sadmin/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SadminConfig(AppConfig):
-    name = 'sadmin'
--- univhub/univhub/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.conf.urls import url
-from django.contrib import admin
-from django.conf.urls import include
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'sadmin/',include('sadmin.urls')),
-]

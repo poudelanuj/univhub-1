4a36230f71846b21cd74bb3bbdaca8b51566eb59
enish paneru <theboyenish@gmail.com>
2017-10-04 10:46:42 +0545

    corrected errornous directories

---
+++ univhub/sadmin/urls.py
@@ -0,0 +1,10 @@
+
+from django.conf.urls import url
+from django.contrib import admin
+from django.conf.urls import include
+from . import views
+
+urlpatterns = [
+    url(r'^', views.index),
+    url(r'notifications.html',views.notifications),
+]

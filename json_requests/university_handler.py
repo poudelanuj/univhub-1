b135714832e1a419081b468e00a62bca0e676fa5
BasantaChaulagain <basanta83chaulagain@gmail.com>
2017-10-07 12:34:46 +0545

    Before merging from Bipin

--- json_requests/university_handler.py
+++ json_requests/university_handler.py
@@ -2,5 +2,6 @@ from sadmin.models import UniAddress
 from sadmin.models import University
 from sadmin.models import UniversityContent
 from sadmin.models import UniversityRequirement
+
 def basic_inof_university(request):
     pass
